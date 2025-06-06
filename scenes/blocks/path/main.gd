0943e5d2436251fe9f0cc8b2756e0d6a296f7f8c
rsa17826 <rsa17826@email.vccs.edu>
2025-06-06 17:54:29 -0400

    NO MESSAGE SET

--- scenes/blocks/path/main.gd
+++ scenes/blocks/path/main.gd
@@ -6,18 +6,27 @@ func generateBlockOpts():
 
 func on_respawn():
   await global.wait()
-  for follower in $Area2D.get_overlapping_areas():
-    log.pp(follower.root.id)
-    var pathFollow = preload("res://scenes/blocks/path/pf.tscn").instantiate()
-    $Path2D.curve = Curve2D.new()
-    var pathinfo = (selectedOptions.path.split(",") as Array).map(func(e):
-      return float(e))
-    while len(pathinfo):
-      $Path2D.curve.add_point(Vector2(pathinfo.pop_front(), pathinfo.pop_front()))
+  # for follower in $Area2D.get_overlapping_areas():
+  #   log.pp(block.id)
+  $Path2D.curve = Curve2D.new()
+  var pathinfo = (selectedOptions.path.split(",") as Array).map(func(e):
+    return float(e))
+  log.pp(pathinfo)
+  while len(pathinfo):
+    var pos = Vector2(pathinfo.pop_front(), pathinfo.pop_front())
+    $Path2D.curve.add_point(pos)
+    for block in global.level.get_children():
+      if block == self: return
+      if not block is Node2D: return
+      if block.global_position == pos:
+        addToPath(block)
 
-    $Path2D.add_child(pathFollow)
-    follower.root.reparent(pathFollow)
-    # follower.root.global_position = pathFollow.global_position
-    # follower.root.position = Vector2.ZERO
-    # await global.wait()
-    # follower.root.position = Vector2.ZERO
+func addToPath(block):
+  block.curve = $Path2D.curve
+  var pathFollow = preload("res://scenes/blocks/path/pf.tscn").instantiate()
+  $Path2D.add_child(pathFollow)
+  block.reparent(pathFollow)
+  # block.global_position = pathFollow.global_position
+  block.position = Vector2.ZERO
+  await global.wait()
+  block.position = Vector2.ZERO
