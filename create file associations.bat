765d1b3adbdbe2982d20f7f0777cfca029668a50
rsa17826 <rsa17826@email.vccs.edu>
2025-06-10 18:35:30 -0400

    NO MESSAGE SET

---
+++ create file associations.bat
@@ -0,0 +1,25 @@
+@echo off
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+setlocal
+
+rem Define the file extension
+set "fileExtension=vex++"
+
+rem Get the path to the current directory where the script is located
+set "appPath=%~dp0launcher.exe"
+
+rem Create the file association
+assoc .%fileExtension%=%fileExtension%File
+ftype %fileExtension%File="%appPath%" "offline %%1"
+
+rem Optional: Display a message to confirm the association
+echo File association for .%fileExtension% created with %appPath%.
+
+endlocal
--- launcher/launcher.ahk
+++ launcher/launcher.ahk
@@ -1,4 +1,5 @@
-﻿#Requires AutoHotkey v2.0
+﻿;@Ahk2Exe-SetMainIcon ../pole.ico
+#Requires AutoHotkey v2.0
 #SingleInstance Force
 
 #Include *i <AutoThemed>
