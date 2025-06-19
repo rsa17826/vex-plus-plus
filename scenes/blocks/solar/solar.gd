2b4f4bb914fad6c16ea44a4910f07f42e45e39a2
rsa17826 <rsa17826@email.vccs.edu>
2025-06-19 17:45:48 -0400

    NO MESSAGE SET

--- scenes/blocks/solar/solar.gd
+++ scenes/blocks/solar/solar.gd
@@ -3,7 +3,6 @@ extends EditorBlock
 class_name BlockSolar
 
 func on_physics_process(delta: float) -> void:
-  log.pp(global.player.lightsOut)
   if global.player.lightsOut:
     __disable()
   else:
