caaac56d6651a9d9a8f931d2ad49bf8aca895c2d
rsa17826 <rsa17826@email.vccs.edu>
2025-07-08 11:27:26 -0400

    NO MESSAGE SET

--- addons/fancy_folder_icons/plugin.gd
+++ addons/fancy_folder_icons/plugin.gd
@@ -7,63 +7,117 @@ extends EditorPlugin
 #	https://github.com/CodeNameTwister/Fancy-Folder-Icons
 #	author:	"Twister"
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-const DOT_USER : String = "user://editor/fancy_folder_icons.dat"
+const DOT_USER: String = "user://editor/fancy_folder_icons.dat"
 
-var _buffer : Dictionary = {}
-var _tree : Tree = null
-var _busy : bool = false
+var _buffer: Dictionary = {}
+var _tree: Tree = null
+var _busy: bool = false
 
-var _menu_service : EditorContextMenuPlugin = null
-var _popup : Window = null
+var _menu_service: EditorContextMenuPlugin = null
+var _popup: Window = null
 
-var _tchild : TreeItem = null
-var _tdelta : int = 0
+var _tchild: TreeItem = null
+var _tdelta: int = 0
 
-var _docky : Docky = null
+var _docky: Docky = null
 
-var size : Vector2 = Vector2(12.0, 12.0)
+var size: Vector2 = Vector2(12.0, 12.0)
 
-var _is_saving : bool = false
+var _is_saving: bool = false
 
 func get_buffer() -> Dictionary:
   return _buffer
 
 class Docky extends RefCounted:
-  var drawing : bool = false
-  var dock : ItemList = null
+  var drawing: bool = false
+  var dock: ItemList = null
   
-  var plugin : Object = null
+  var plugin: Object = null
   
-  func _init(set_plugin : Object) -> void:
+  func _init(set_plugin: Object) -> void:
     plugin = set_plugin
   
+  func matches(key, item, retval):
+    if key == item:
+      return false
+    var splitKey: Array = (key.split("/") as Array).filter(func(e): return e)
+    var splitItem: Array = (item.split("/") as Array).filter(func(e): return e)
+    if len(splitItem) <= len(splitKey):
+      return false
+    var result: String = ''
+    var itemrep: Array[String] = []
+    if key != "res://scenes/blocks/*/images/":
+      return false
+    log.pp(key, item)
+    for i in range(len(splitItem)):
+      if i not in splitItem or i not in splitKey:
+        log.pp("len ", (splitItem), (splitKey), "i ", i, result)
+        return false
+      if splitKey[i] == splitItem[i]:
+        result += splitKey[i]
+        continue
+      if splitKey[i][0] == '$':
+        var num := int(splitKey[i].substr(1, -1))
+        if num in itemrep:
+          result += itemrep[num] + '/'
+          return
+      if splitKey[i] == '*':
+        itemrep.append(splitItem[i])
+        result += splitItem[i] + '/'
+        continue
+      else:
+        return false
+    log.pp("result:", result, itemrep, splitKey, splitItem, key, item, retval)
+    return result
+
   func update_icons() -> void:
     if !dock:
       return
-    var buffer : Dictionary = plugin.get_buffer()
-    var mt : Dictionary = {}
+    var buffer: Dictionary = plugin.get_buffer()
+    buffer['res://scenes/blocks/*/images/'] = "res://scenes/blocks/image.png"
+    var mt: Dictionary = {}
     
-    for x : int in dock.item_count:
-      var data : String = str(dock.get_item_metadata(x))
+    for x: int in dock.item_count:
+      var data: String = str(dock.get_item_metadata(x))
       mt[data] = [x, 0]
-    
-    for key : String in buffer.keys():
-      for m : String in mt.keys():
+    # log.pp(mt, buffer)
+    for key: String in buffer.keys():
+      for m: String in mt.keys():
         if m == key:
-          mt[m][1] = m.length() + 1
-          dock.set_item_icon(mt[m][0], buffer[key])
+          continue
+        #   # log.pp("same", key, m)
+        #   mt[m][1] = m.length() + 1
+        #   dock.set_item_icon(mt[m][0], buffer[key])
+        #   continue
+        # if matches(key, m, buffer[key]):
+        #   log.pp("matched", key, m, buffer[key])
+        #   if buffer[key] is ImageTexture:
+        #     mt[m][1] = m.length() + 1
+        #     # dock.set_item_icon(mt[m][0], buffer[key])
+        #     continue
+        #   var texture_path = matches(key, m, buffer[key])
+        #   var im = Image.new()
+        #   im.load(texture_path)
+        #   var tx: Texture2D = ImageTexture.create_from_image(im)
+        #   mt[m][1] = m.length() + 1
+        #   var size: Vector2 = Vector2(12.0, 12.0)
+        #   print("Image selected '", texture_path.get_file(), "' size: ", tx.get_size(), " resized to ", size.x, "x", size.y)
+        #   var img: Image = tx.get_image()
+        #   img.resize(int(size.x), int(size.y))
+        #   tx = ImageTexture.create_from_image(img)
+          # dock.set_item_icon(mt[m][0], tx)
           
-        elif m.get_extension().is_empty() and m.begins_with(key):
-          var l : int = key.length()
-          if mt[m][1] < l:
-            mt[m][1] = l
-            dock.set_item_icon(mt[m][0], buffer[key])
+        # elif m.get_extension().is_empty() and m.begins_with(key):
+        #   var l: int = key.length()
+        #   if mt[m][1] < l:
+        #     mt[m][1] = l
+        #     dock.set_item_icon(mt[m][0], buffer[key])
     _dispose.call_deferred()
     return
     
   func _dispose() -> void:
-    var o : Variant = self
-    for __ : int in range(2):
+    var o: Variant = self
+    for __: int in range(2):
       await Engine.get_main_loop().process_frame
     if is_instance_valid(o):
       o.set_deferred(&"drawing", false)
@@ -74,7 +128,7 @@ class Docky extends RefCounted:
     drawing = true
     update_icons.call_deferred()
   
-  func update(new_dock : ItemList) -> void:
+  func update(new_dock: ItemList) -> void:
     dock = new_dock
     
     if !dock.draw.is_connected(_on_change):
@@ -84,32 +138,32 @@ class Docky extends RefCounted:
       #var icon : Texture2D = dock.get_item_icon(0)
 
 func _setup() -> void:
-  var dir : String = DOT_USER.get_base_dir()
+  var dir: String = DOT_USER.get_base_dir()
   if !DirAccess.dir_exists_absolute(dir):
     DirAccess.make_dir_recursive_absolute(dir)
     return
   if FileAccess.file_exists(DOT_USER):
-    var cfg : ConfigFile = ConfigFile.new()
-    if OK != cfg.load(DOT_USER):return
+    var cfg: ConfigFile = ConfigFile.new()
+    if OK != cfg.load(DOT_USER): return
     _buffer = cfg.get_value("DAT", "PTH", {})
 
 func _quick_save() -> void:
   if FileAccess.file_exists(DOT_USER):
-    var cfg : ConfigFile = ConfigFile.new()
-    if OK != cfg.load(DOT_USER):return
+    var cfg: ConfigFile = ConfigFile.new()
+    if OK != cfg.load(DOT_USER): return
     cfg.set_value("DAT", "PTH", _buffer)
     cfg = null
-  set_deferred(&"_is_saving" , false)
+  set_deferred(&"_is_saving", false)
 
 #region callbacks
-func _moved_callback(a0 : String, b0 : String ) -> void:
+func _moved_callback(a0: String, b0: String) -> void:
   if a0 != b0:
     if _buffer.has(a0):
       _buffer[b0] = _buffer[a0]
       _buffer.erase(a0)
       save_queue()
 
-func _remove_callback(path : String) -> void:
+func _remove_callback(path: String) -> void:
   if _buffer.has(path):
     _buffer.erase(path)
     save_queue()
@@ -119,20 +173,20 @@ func _def_update() -> void:
   update.call_deferred()
 
 func update() -> void:
-  if _buffer.size() == 0:return
-  if _busy:return
+  if _buffer.size() == 0: return
+  if _busy: return
   _busy = true
-  var root : TreeItem = _tree.get_root()
-  var item : TreeItem = root.get_first_child()
+  var root: TreeItem = _tree.get_root()
+  var item: TreeItem = root.get_first_child()
 
   while null != item and item.get_metadata(0) != "res://":
     item = item.get_next()
 
   if _enable_icons_on_split:
-    var dock : ItemList = get_docky()
+    var dock: ItemList = get_docky()
     if dock:
       if !is_instance_valid(_docky):
-        _docky = Docky.new(self)
+        _docky = Docky.new(self )
       _docky.update(dock)
     elif is_instance_valid(_docky):
       _docky = null
@@ -146,37 +200,104 @@ func update() -> void:
     
   set_deferred(&"_busy", false)
 
-func _explore(item : TreeItem, texture : Texture2D = null, as_root : bool = true) -> void:
-  var meta : String = str(item.get_metadata(0))
+func matches(key, item, retval):
+  if key == item:
+    log.pp(item)
+    return retval
+  var splitKey: Array = (key.split("/") as Array).filter(func(e): return e)
+  var splitItem: Array = (item.split("/") as Array).filter(func(e): return e)
+  if len(splitItem) < len(splitKey):
+    return false
+  var result: String = ''
+  var itemrep: Array[String] = []
+  for i in range(0, len(splitItem)):
+    if i >= len(splitItem) or i >= len(splitKey):
+      # log.pp("len ", key, item, retval, "i ", i, result, splitItem[i])
+      return false
+    if splitKey[i] == splitItem[i]:
+      result += splitKey[i] + '/'
+      # log.pp(result)
+      continue
+    if splitKey[i][0] == '$':
+      var num := int(splitKey[i].substr(1, -1))
+      if num in itemrep:
+        result += itemrep[num] + '/'
+        # log.pp(result)
+        continue
+    if splitKey[i] == '**':
+      itemrep.append(splitItem[i])
+      result += splitItem[i] + '/'
+      # log.pp(result)
+      continue
+    if splitKey[i] == '*':
+      itemrep.append(splitItem[i])
+      result += splitItem[i] + '/'
+      # log.pp(result)
+      continue
+    return false
+  # log.pp("result:", result, itemrep, splitKey, splitItem, key, item, retval)
+  var newRetVal = []
+  for part in retval.split("/"):
+    for i in range(0, len(itemrep)):
+      if part == "$" + str(i):
+        newRetVal.append(itemrep[i])
+      else:
+        newRetVal.append(part)
+
+  return "/".join(newRetVal)
+
+func _explore(item: TreeItem, texture: Texture2D = null, as_root: bool = true) -> void:
+  var meta: String = str(item.get_metadata(0))
   if _buffer.has(meta):
     texture = _buffer[meta]
     as_root = true
 
+  # texture = null
+  
+  for key in _buffer:
+    if matches(key, meta, _buffer[key]):
+      # log.pp("matched", key, meta, _buffer[key])
+      if _buffer[key] is ImageTexture:
+        texture = _buffer[key]
+        continue
+      var texture_path = matches(key, meta, _buffer[key])
+
+      var im = Image.new()
+      im.load(texture_path)
+      var tx: Texture2D = ImageTexture.create_from_image(im)
+      # print("Image selected '", texture_path.get_file(), "' size: ", tx.get_size(), " resized to ", size.x, "x", size.y)
+      var img: Image = tx.get_image()
+      img.resize(int(size.x), int(size.y))
+      tx = ImageTexture.create_from_image(img)
+      texture = tx
+      break
+      # dock.set_item_icon(mt[m][0], tx)
+              
   if texture != null:
     if as_root or !FileAccess.file_exists(meta):
       item.set_icon(0, texture)
 
-  for i : TreeItem in item.get_children():
+  for i: TreeItem in item.get_children():
     _explore(i, texture, false)
 
 func _get_dummy_tree_node() -> void:
   set_physics_process(false)
-  var root : TreeItem = _tree.get_root()
+  var root: TreeItem = _tree.get_root()
   if root:
     _tchild = root.get_first_child()
   if is_instance_valid(_tchild):
-    var icon_size : Texture2D = _tchild.get_icon(0)
+    var icon_size: Texture2D = _tchild.get_icon(0)
     if icon_size:
       size = icon_size.get_size()
     set_physics_process(true)
 
-func _on_select_texture(tx : Texture2D, texture_path : String, paths : PackedStringArray) -> void:
+func _on_select_texture(tx: Texture2D, texture_path: String, paths: PackedStringArray) -> void:
   if tx.get_size() != size:
     print("Image selected '", texture_path.get_file(), "' size: ", tx.get_size(), " resized to ", size.x, "x", size.y)
-    var img : Image = tx.get_image()
+    var img: Image = tx.get_image()
     img.resize(int(size.x), int(size.y))
     tx = ImageTexture.create_from_image(img)
-  for p : String in paths:
+  for p: String in paths:
     _buffer[p] = tx
   _def_update()
   save_queue()
@@ -187,16 +308,16 @@ func save_queue() -> void:
   _is_saving = true
   _quick_save.call_deferred()
 
-func _on_reset_texture(paths : PackedStringArray) -> void:
-  for p : String in paths:
+func _on_reset_texture(paths: PackedStringArray) -> void:
+  for p: String in paths:
     if _buffer.has(p):
       _buffer.erase(p)
-  var fs : EditorFileSystem = EditorInterface.get_resource_filesystem()
+  var fs: EditorFileSystem = EditorInterface.get_resource_filesystem()
   if fs: fs.filesystem_changed.emit()
 
-func _on_iconize(paths : PackedStringArray) -> void:
-  const PATH : String = "res://addons/fancy_folder_icons/scene/icon_selector.tscn"
-  var pop : Window = get_node_or_null("_POP_ICONIZER_")
+func _on_iconize(paths: PackedStringArray) -> void:
+  const PATH: String = "res://addons/fancy_folder_icons/scene/icon_selector.tscn"
+  var pop: Window = get_node_or_null("_POP_ICONIZER_")
   if pop == null:
     pop = (ResourceLoader.load(PATH) as PackedScene).instantiate()
     pop.name = "_POP_ICONIZER_"
@@ -211,17 +332,16 @@ func _on_iconize(paths : PackedStringArray) -> void:
   pop.popup_centered()
 
 func get_docky() -> ItemList:
-  var out : ItemList = null
-  var dock : Control = EditorInterface.get_file_system_dock()
+  var out: ItemList = null
+  var dock: Control = EditorInterface.get_file_system_dock()
   if dock:
     out = dock.find_child("*FileSystemList*", true, false)
   return out
   
-
 func _ready() -> void:
   set_physics_process(false)
-  var dock : FileSystemDock = EditorInterface.get_file_system_dock()
-  var fs : EditorFileSystem = EditorInterface.get_resource_filesystem()
+  var dock: FileSystemDock = EditorInterface.get_file_system_dock()
+  var fs: EditorFileSystem = EditorInterface.get_resource_filesystem()
   _n(dock)
 
   _get_dummy_tree_node()
@@ -237,19 +357,18 @@ func _ready() -> void:
 
   _def_update()
 
-
-var _enable_icons_on_split : bool = true
+var _enable_icons_on_split: bool = true
 func _enter_tree() -> void:
   _setup()
 
   _menu_service = ResourceLoader.load("res://addons/fancy_folder_icons/menu_fancy.gd").new()
   _menu_service.iconize_paths.connect(_on_iconize)
   
-  var vp : Viewport = Engine.get_main_loop().root
+  var vp: Viewport = Engine.get_main_loop().root
   vp.focus_entered.connect(_on_wnd)
   vp.focus_exited.connect(_out_wnd)
   
-  var editor : EditorSettings = EditorInterface.get_editor_settings()
+  var editor: EditorSettings = EditorInterface.get_editor_settings()
   if editor:
     editor.settings_changed.connect(_on_change_settings)
     if !editor.has_setting("plugin/fancy_folder_icons/enable_icons_on_split"):
@@ -258,9 +377,9 @@ func _enter_tree() -> void:
       _enable_icons_on_split = editor.get_setting("plugin/fancy_folder_icons/enable_icons_on_split")
 
 func _on_change_settings() -> void:
-  var editor : EditorSettings = EditorInterface.get_editor_settings()
+  var editor: EditorSettings = EditorInterface.get_editor_settings()
   if editor:
-    var settings : PackedStringArray = editor.get_changed_settings()
+    var settings: PackedStringArray = editor.get_changed_settings()
     if "plugin/fancy_folder_icons/enable_icons_on_split" in settings:
       _enable_icons_on_split = editor.get_setting("plugin/fancy_folder_icons/enable_icons_on_split")
       
@@ -271,8 +390,8 @@ func _exit_tree() -> void:
   if is_instance_valid(_menu_service):
     remove_context_menu_plugin(_menu_service)
 
-  var dock : FileSystemDock = EditorInterface.get_file_system_dock()
-  var fs : EditorFileSystem = EditorInterface.get_resource_filesystem()
+  var dock: FileSystemDock = EditorInterface.get_file_system_dock()
+  var fs: EditorFileSystem = EditorInterface.get_resource_filesystem()
   if dock.files_moved.is_connected(_moved_callback):
     dock.files_moved.disconnect(_moved_callback)
   if dock.folder_moved.is_connected(_moved_callback):
@@ -286,14 +405,13 @@ func _exit_tree() -> void:
   if fs.filesystem_changed.is_connected(_def_update):
     fs.filesystem_changed.disconnect(_def_update)
 
-  var editor : EditorSettings = EditorInterface.get_editor_settings()
+  var editor: EditorSettings = EditorInterface.get_editor_settings()
   if editor:
     editor.settings_changed.disconnect(_on_change_settings)
   
-
   #region user_dat
-  var cfg : ConfigFile = ConfigFile.new()
-  for k : String in _buffer.keys():
+  var cfg: ConfigFile = ConfigFile.new()
+  for k: String in _buffer.keys():
     if !DirAccess.dir_exists_absolute(k) and !FileAccess.file_exists(k):
       _buffer.erase(k)
       continue
@@ -308,17 +426,17 @@ func _exit_tree() -> void:
   if !fs.is_queued_for_deletion():
     fs.filesystem_changed.emit()
     
-  var vp : Viewport = Engine.get_main_loop().root
+  var vp: Viewport = Engine.get_main_loop().root
   vp.focus_entered.disconnect(_on_wnd)
   vp.focus_exited.disconnect(_out_wnd)
   
-func _on_wnd() -> void:set_physics_process(true)
-func _out_wnd() -> void:set_physics_process(false)
+func _on_wnd() -> void: set_physics_process(true)
+func _out_wnd() -> void: set_physics_process(false)
 
 #region rescue_fav
-func _n(n : Node) -> bool:
+func _n(n: Node) -> bool:
   if n is Tree:
-    var t : TreeItem = (n.get_root())
+    var t: TreeItem = (n.get_root())
     if null != t:
       t = t.get_first_child()
       while t != null:
