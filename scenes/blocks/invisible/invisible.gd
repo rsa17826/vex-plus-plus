efccdca8852a8c70427a71512924c372f9c3c248
rsa17826 <rsa17826@email.vccs.edu>
2025-07-17 08:23:41 -0400

    NO MESSAGE SET

--- scenes/blocks/invisible/invisible.gd
+++ scenes/blocks/invisible/invisible.gd
@@ -2,29 +2,21 @@
 extends EditorBlock
 class_name BlockInvisible
 
+@export var sprite: Sprite2D
+
 func on_physics_process(delta: float) -> void:
-  var top = thingThatMoves.global_position.y + (sizeInPx.y / 2)
-  var left = thingThatMoves.global_position.x + (sizeInPx.x / 2)
-  var right = thingThatMoves.global_position.x - (sizeInPx.x / 2)
-  var bottom = thingThatMoves.global_position.y - (sizeInPx.y / 2)
+  var top = thingThatMoves.global_position.y - (sizeInPx.y / 2)
+  var left = thingThatMoves.global_position.x - (sizeInPx.x / 2)
+  var right = thingThatMoves.global_position.x + (sizeInPx.x / 2)
+  var bottom = thingThatMoves.global_position.y + (sizeInPx.y / 2)
   var pos = global.player.global_position
 
   # Clamp position to rectangle bounds
   var closest_x = clamp(pos.x, left, right)
   var closest_y = clamp(pos.y, top, bottom)
 
-  # If pos is outside the rectangle, find closest point
-  if pos.x < left:
-    closest_x = left
-  elif pos.x > right:
-    closest_x = right
-
-  if pos.y < top:
-    closest_y = top
-  elif pos.y > bottom:
-    closest_y = bottom
-
   # Calculate distance between pos and closest point
   var closest_point = Vector2(closest_x, closest_y)
   var distance = pos.distance_to(closest_point)
-  log.pp(distance)
+  var newval = clamp(global.rerange(distance, 70, 700, 0.0, 1.0), 0.0, 1.0)
+  sprite.self_modulate.a = lerp(sprite.self_modulate.a, newval, 0.2)
