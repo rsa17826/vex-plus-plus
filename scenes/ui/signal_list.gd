eb85972dd5df813811279de927d0b8484e7a2b65
rsa17826 <rsa17826@email.vccs.edu>
2025-07-27 14:15:35 -0400

    NO MESSAGE SET

--- scenes/ui/signal_list.gd
+++ scenes/ui/signal_list.gd
@@ -2,39 +2,38 @@ extends VBoxContainer
 
 func _ready():
   global.onSignalChanged(onSignalChanged)
-func _process(delta):
-  global.onSignalChanged(onSignalChanged)
-var signalList := {}
+
+var listOfLoadedSignals := {}
 
 const ON = preload("res://scenes/ui/images/on.png")
 const OFF = preload("res://scenes/ui/images/off.png")
 
 func onSignalChanged(id, on):
-  if id not in signalList:
-    signalList[id] = $signalDisplay.duplicate()
+  if id not in listOfLoadedSignals:
+    listOfLoadedSignals[id] = $signalDisplay.duplicate()
     # var label := Label.new()
     # label.name = "signal id"
-    # signalList[id].add_child(label)
-    add_child(signalList[id])
-    signalList[id].visible = true
-    signalList[id].get_node("signal id").text = str(id)
+    # listOfLoadedSignals[id].add_child(label)
+    add_child(listOfLoadedSignals[id])
+    listOfLoadedSignals[id].visible = true
+    listOfLoadedSignals[id].get_node("signal id").text = 'ID: ' + str(id)
 
-  var blockImages := {}
-  signalList[id].get_node("image").texture = ON if on else OFF
-  signalList[id].get_node("count").text = str(len(global.activeSignals[id]))
+  var blockData := {}
+  listOfLoadedSignals[id].get_node("image").texture = ON if on else OFF
+  listOfLoadedSignals[id].get_node("count").text = str(len(global.activeSignals[id]))
   for block in global.activeSignals[id]:
-    if block.id not in blockImages:
-      blockImages[block.id] = {'count': 0, 'ghost': block.ghost}
-    blockImages[block.id].count += 1
+    if block.id not in blockData:
+      blockData[block.id] = {'count': 0, 'ghost': block.ghost}
+    blockData[block.id].count += 1
 
-  for blockImageNode in signalList[id].blockImages:
+  for blockImageNode in listOfLoadedSignals[id].blockImages:
     blockImageNode.queue_free()
-  signalList[id].blockImages = []
+  listOfLoadedSignals[id].blockImages = []
 
-  for blockId in blockImages:
+  for i in blockData:
     var blockImageNode = $signalDisplay.duplicate()
     blockImageNode.visible = true
-    signalList[id].add_child(blockImageNode)
-    signalList[id].blockImages.append(blockImageNode)
-    blockImageNode.get_node("count").text = str(blockImages[blockId].count)
-    blockImageNode.get_node("image").texture = blockImages[blockId].ghost.texture
+    listOfLoadedSignals[id].add_child(blockImageNode)
+    listOfLoadedSignals[id].blockImages.append(blockImageNode)
+    blockImageNode.get_node("count").text = str(blockData[i].count)
+    blockImageNode.get_node("image").texture = blockData[i].ghost.texture
