e41827249d64894afa66af33f30745b87cb4b312
rsa17826 <rsa17826@email.vccs.edu>
2025-08-11 12:58:50 -0400

    NO MESSAGE SET

--- scenes/input_remapping/input_options_menu.gd
+++ scenes/input_remapping/input_options_menu.gd
@@ -5,3 +5,6 @@ func _input(event: InputEvent) -> void:
   if event is InputEventKey:
     if Input.is_action_just_pressed("show_keybinds", true):
       visible = !visible
+
+func close():
+  visible = false
