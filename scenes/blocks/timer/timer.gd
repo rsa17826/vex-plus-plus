1f113f28788ceccd71a0e3ba9206bba92fcd3c64
rsa17826 <rsa17826@email.vccs.edu>
2025-08-24 22:31:03 -0400

    NO MESSAGE SET

--- scenes/blocks/timer/timer.gd
+++ scenes/blocks/timer/timer.gd
@@ -14,8 +14,12 @@ var lastOn = false
 func generateBlockOpts():
   blockOptions.signalInputId = {"type": global.PromptTypes.int, "default": 0}
   blockOptions.chargeTime = {"type": global.PromptTypes.float, "default": 1}
+  blockOptions.dontStopCharging = {"type": global.PromptTypes.bool, "default": false}
+  blockOptions.clearChargeProgressSignalDeactivation = {"type": global.PromptTypes.bool, "default": false}
   blockOptions.signalOutputId = {"type": global.PromptTypes.int, "default": 0}
   blockOptions.dischargeTime = {"type": global.PromptTypes.float, "default": 1}
+  blockOptions.dontStopDischarging = {"type": global.PromptTypes.bool, "default": false}
+  blockOptions.clearDischargeProgressSignalActivation = {"type": global.PromptTypes.bool, "default": false}
 
 func on_respawn():
   chargeState = States.discharged
@@ -27,7 +31,7 @@ func on_respawn():
 
 func onSignalChanged(id, on, callers):
   breakpoint
-  if self in callers: return
+  # if self in callers: return
   if id == selectedOptions.signalInputId:
     if lastOn == on: return
     lastOn = on
@@ -36,10 +40,16 @@ func onSignalChanged(id, on, callers):
         chargeTimer = 0
         chargeState = States.charging
       if chargeState == States.discharging:
+        if selectedOptions.dontStopDischarging: return
+        if selectedOptions.clearDischargeProgressSignalActivation:
+          chargeTimer = selectedOptions.dischargeTime
         chargeTimer = global.rerange(chargeTimer, 0, selectedOptions.dischargeTime, 0, selectedOptions.chargeTime)
         chargeState = States.charging
     else:
       if chargeState == States.charging:
+        if selectedOptions.dontStopCharging: return
+        if selectedOptions.clearChargeProgressSignalDeactivation:
+          chargeTimer = 0
         chargeTimer = global.rerange(chargeTimer, 0, selectedOptions.chargeTime, 0, selectedOptions.dischargeTime)
         chargeState = States.discharging
       if chargeState == States.charged:
@@ -65,10 +75,14 @@ func on_physics_process(delta: float) -> void:
       chargeTimer += delta
       if chargeTimer >= selectedOptions.chargeTime:
         chargeState = States.charged
+        if !lastOn:
+          chargeTimer = global.rerange(chargeTimer, 0, selectedOptions.chargeTime, 0, selectedOptions.dischargeTime)
+          chargeState = States.discharging
         global.sendSignal(selectedOptions.signalOutputId, self , true)
     States.discharging:
-      if chargeTimer > 0:
-        chargeTimer -= delta
-        if chargeTimer <= 0:
-          chargeState = States.discharged
-          global.sendSignal(selectedOptions.signalOutputId, self , false)
+      chargeTimer -= delta
+      if chargeTimer <= 0:
+        chargeState = States.discharged
+        if lastOn:
+          chargeState = States.charging
+        global.sendSignal(selectedOptions.signalOutputId, self , false)
