408fa5f3a79f58d9b049617c684cff9682a17f06
rsa17826 <rsa17826@email.vccs.edu>
2025-08-27 21:07:00 -0400

    NO MESSAGE SET

--- GLOBAL/custom nodes/NestedSearchable/nested_searchable.gd
+++ GLOBAL/custom nodes/NestedSearchable/nested_searchable.gd
@@ -2,7 +2,6 @@ extends Control
 class_name NestedSearchable
 
 @export var thisText: String
-@export var SPLITTER := "\u1562"
 
 func updateSearch(search: String, parentText: String = ''):
   visible = false
@@ -10,18 +9,16 @@ func updateSearch(search: String, parentText: String = ''):
     if item is NestedSearchable:
       item.updateSearch(search, thisText)
   if search in parentText:
-    visible = true
     showChildren()
   if search in thisText:
-    visible = true
     var node = self
     while 1:
+      node.visible = true
       node = node.get_parent()
       if !node or (not (node is NestedSearchable)): break
-      node.visible = true
 
 func showChildren():
+  visible = true
   for item in get_children():
     if item is NestedSearchable:
-      item.visible = true
       item.showChildren()
