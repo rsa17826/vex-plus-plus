0e34912630dad2189c20423db5bf4698e0c58991
rsa17826 <rsa17826@email.vccs.edu>
2025-08-31 10:47:44 -0400

    NO MESSAGE SET

--- scenes/hitbox scripts/__HitboxTypes.gd
+++ scenes/hitbox scripts/__HitboxTypes.gd
@@ -8,15 +8,22 @@ enum Types {
   death
 }
 
-var hitboxType: Types
+var hitboxType: Types:
+  set(val):
+    hitboxType = val
+    updateColor()
 
-func _init():
+func _ready() -> void:
+  updateColor()
+
+func updateColor() -> void:
   match hitboxType:
     Types.attachDetector:
-      self.debug_color = Color('df57006b')
+      self.debug_color = Color('#df57006b')
     Types.solid:
-      self.debug_color = Color('0099b36b')
+      self.debug_color = Color('#0099b36b')
     Types.area:
-      self.debug_color = Color(0, 0, 1)
+      self.debug_color = Color('#00ff006b')
     Types.death:
-      self.debug_color = Color('ff00006b')
+      self.debug_color = Color('#ff00006b')
+      # 0.4196078431
