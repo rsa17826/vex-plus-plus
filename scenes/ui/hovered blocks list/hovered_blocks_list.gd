5ee50cf3884cfbee1b28bac6b524788649bfaa3d
rsa17826 <rsa17826@email.vccs.edu>
2025-09-02 22:18:06 -0400

    NO MESSAGE SET

--- scenes/ui/hovered blocks list/hovered_blocks_list.gd
+++ scenes/ui/hovered blocks list/hovered_blocks_list.gd
@@ -23,8 +23,8 @@ func _process(delta: float) -> void:
     labels.append(l)
     listElem.add_child(l)
   if global.selectedBlock:
-    %selectedBlock.text = global.selectedBlock.id
-    %selectedBlock.visible = true
+    %selectedBlock.text = getBlockData(global.selectedBlock, true)
+    %selectedBlock.visible = !!%selectedBlock.text
   else:
     %selectedBlock.visible = false
   %HSeparator.visible = blocks and global.selectedBlock
@@ -34,6 +34,36 @@ func _process(delta: float) -> void:
       label.visible = false
     else:
       label.visible = true
-      labels[i].text = blocks[i].id
+      labels[i].text = getBlockData(blocks[i], false)
+      if not label.text:
+        label.visible = false
     i += 1
   ttm.position = get_local_mouse_position() + Vector2(20, 20)
+
+func getBlockData(block, isSelected):
+  return (getId(block, isSelected) + getPos(block, isSelected) + getRot(block, isSelected)).strip_edges()
+
+func getId(block, isSelected):
+  if isSelected:
+    if not global.useropts.showSelectedBlockId:
+      return ''
+  else:
+    if not global.useropts.showHoveredBlockId:
+      return ''
+  return block.id
+func getPos(block, isSelected):
+  if isSelected:
+    if not global.useropts.showSelectedBlockPosition:
+      return ''
+  else:
+    if not global.useropts.showHoveredBlockPosition:
+      return ''
+  return " (" + str(int(block.startPosition.x)) + ', ' + str(int(block.startPosition.y)) + ")"
+func getRot(block, isSelected):
+  if isSelected:
+    if not global.useropts.showSelectedBlockRotation:
+      return ''
+  else:
+    if not global.useropts.showHoveredBlockRotation:
+      return ''
+  return " R" + str(int(block.startRotation_degrees))
