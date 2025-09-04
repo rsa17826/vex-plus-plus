d62b787486922e35b72b95e2dca471793f6f34fd
rsa17826 <rsa17826@email.vccs.edu>
2025-09-04 19:42:08 -0400

    NO MESSAGE SET

--- scenes/main menu/open in correct version.gd
+++ scenes/main menu/open in correct version.gd
@@ -6,8 +6,13 @@ var levelName: String
 func _on_button_open_in_correct_version_pressed() -> void:
   if DirAccess.remove_absolute(global.path.abs("res://process")):
     DirAccess.remove_absolute(global.path.abs("res://process"))
-  OS.create_process(r"..\..\vex++.exe", PackedStringArray([
-    "version", str(version), "silent", "--loadMap", levelName
-  ]))
+  if FileAccess.file_exists(r"..\..\vex++.exe"):
+    OS.create_process(r"..\..\vex++.exe", PackedStringArray([
+      "version", str(version), "silent", "--loadMap", levelName
+    ]))
+  else:
+    OS.create_process(r"..\..\vex++.cmd", PackedStringArray([
+      "version", str(version), "silent", "--loadMap", levelName
+    ]))
   global.quitGame()
   # OS.kill(OS.get_process_id())
