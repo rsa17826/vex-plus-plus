635536b797b8d9bffe4d378c7d9c0f055ba079f6
rsa17826 <rsa17826@email.vccs.edu>
2025-09-21 12:40:55 -0400

    NO MESSAGE SET

--- scenes/blocks/attach detector.gd
+++ scenes/blocks/attach detector.gd
@@ -11,6 +11,20 @@ func _ready() -> void:
   if not Engine.is_editor_hint():
     global.player.Alltryaddgroups.connect(tryaddgroups)
     global.hitboxTypesChanged.connect(updateColor)
+  if not root:
+    var parent = self
+    while parent and 'id' not in parent:
+      parent = parent.get_parent()
+    log.err("root not set", name, parent.id)
+    breakpoint
+  if root.canAttachToPaths:
+    if not ('canAttachToPaths' in root.selectedOptions):
+      log.warn('canAttachToPaths', root.id)
+      return
+  if root.canAttachToThings:
+    if not ('canAttachToThings' in root.selectedOptions):
+      log.warn('canAttachToThings', root.id)
+      return
   updateColor()
 
 func updateColor() -> void:
@@ -20,43 +34,10 @@ func updateColor() -> void:
   visible = global.useropts.showAttachDetectorHitboxes
   self.modulate = global.useropts.attachDetectorHitboxColor
 
-# func on_respawn():
-#   if not root:
-#     var parent = self
-#     while 'id' not in parent:
-#       parent = parent.get_parent()
-#     log.err("root not set", name, parent.id)
-#     breakpoint
-#   if root.canAttachToPaths:
-#     if not ('canAttachToPaths' in root.selectedOptions):
-#       log.warn('canAttachToPaths', root.id)
-#       return
-#   if root.canAttachToThings:
-#     if not ('canAttachToThings' in root.selectedOptions):
-#       log.warn('canAttachToThings', root.id)
-#       return
-#   if root.canAttachToPaths and root.selectedOptions.canAttachToPaths: pass
-#   elif root.canAttachToThings and root.selectedOptions.canAttachToThings: pass
-#   else: return
-#   following = true
-#   var i = 0
-#   await global.wait()
-#   await global.wait()
-#   while i < 6:
-#     i += 1
-#     await global.wait()
-#     tryaddgroups()
-
 func tryaddgroups():
   enabled = true
   force_shapecast_update()
   enabled = false
-  if not root:
-    var parent = self
-    while 'id' not in parent:
-      parent = parent.get_parent()
-    log.err("root not set", name, parent.id)
-    breakpoint
   for i in range(get_collision_count()):
     var block := get_collider(i)
     block = block.root
