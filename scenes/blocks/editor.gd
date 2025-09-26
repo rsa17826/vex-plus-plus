4dbdf13dc93e74500d4fcb0eb74b29ebb9545796
rsa17826 <rsa17826@email.vccs.edu>
2025-09-26 10:34:07 -0400

    NO MESSAGE SET

--- scenes/blocks/editor.gd
+++ scenes/blocks/editor.gd
@@ -422,23 +422,19 @@ func _physics_process(delta: float) -> void:
   if global.openMsgBoxCount: return
   if (global.selectedBlock == self || self in global.boxSelect_selectedBlocks) && Input.is_action_pressed(&"editor_select"): return
   if _DISABLED and not dontDisablePhysicsProcess: return
-  if not (self is EditorPlayer):
+  if thingThatMoves:
     var lastpos: Vector2 = thingThatMoves.global_position
     for thing in cloneEventsHere:
       if 'on_physics_process' in thing:
         thing.on_physics_process(delta)
     on_physics_process(delta)
-    # if respawning:
-    #   lastMovementStep = Vector2.ZERO
-    # else:
-    if thingThatMoves:
-      lastMovementStep = (
-        thingThatMoves.global_position
-        if thingThatMoves else
-        global_position
-      ) - lastpos
-      if respawning:
-        respawning -= 1
+    lastMovementStep = (
+      thingThatMoves.global_position
+      if thingThatMoves else
+      global_position
+    ) - lastpos
+    if respawning:
+      respawning -= 1
 
     for thing in cloneEventsHere:
       if not thing:
@@ -451,11 +447,6 @@ func _physics_process(delta: float) -> void:
         log.err("no thingThatMoves", block.id)
         breakpoint
       block.thingThatMoves.position += lastMovementStep.rotated(-block.rotation) / block.global_scale
-    # for thing in block.cloneEventsHere:
-    #   if thing is AttachDetector:
-    #     if thing.following:
-    #     else:
-    #       block.unusedOffset += lastMovementStep.rotated(-block.rotation) / block.global_scale
 
 var left_edge: float
 var right_edge: float
